fa5b9e8ff1ba134bcef285a932bb27869c7ca960
mmmarinasel <mmmarinasel@icloud.com>
2022-12-03 21:26:07 +0600

    Initial Commit

---
+++ mmmarinasel_test_task_EffectiveMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mmmarinasel_test_task_EffectiveMobile
+//
+//  Created by Марина Селезнева on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
